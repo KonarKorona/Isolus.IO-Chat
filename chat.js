3b3e60d8c1821685bff5bddef53432cfed52d0dd
Konar Korona <konar@networld.midco.net>
2016-07-15 19:28:32 -0500

    made some changes

--- chat.js
+++ chat.js
@@ -1,17 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var get_ip = require('ipware')().get_ip;
-
-// Broken
 
 app.get('/', function(req,res){
     res.sendfile('chat.html');
 });
 
 io.on('connection', function(socket){
-    console.log("Connection Made");
-    var user = ipfo;
+    console.log("A Stranger Connected!");
     io.emit('chat message', "***** A wild stranger appears! *****");
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
